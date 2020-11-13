bddb119102a2b67f2773a1e25dd5bfa4cd56006c
Arkhean <miens.julien@orange.fr>
2020-11-13 21:12:59 +0100

    série de petites bêtises corrigée

--- pyfit/engine.py
+++ pyfit/engine.py
@@ -1,12 +1,13 @@
 """
 useful function for internal purpose only
 """
+from typing import Any
 import numpy as np
 
-def as_array(x):
+def as_array(x: Any) -> np.ndarray:
     """
     convert list to ndarray if necessary
     """
-    if not x.isinstance(np.ndarray):
+    if not isinstance(x, np.ndarray):
         return np.array(x)
     return x
--- pyfit/loss.py
+++ pyfit/loss.py
@@ -5,7 +5,7 @@ loss functions
 import math
 import numpy as np
 
-from _engine import as_array
+from pyfit.engine import as_array
 
 def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """Mean squared error regression loss"""
@@ -13,15 +13,11 @@ def mean_squared_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     y_pred = as_array(y_pred)
     return np.mean((y_true - y_pred) ** 2)
 
-
-
 def mean_absolute_error(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """Mean absolute error regression loss"""
     y_true = as_array(y_true)
     y_pred = as_array(y_pred)
     return np.mean(np.abs(y_true - y_pred))
-
-
 
 def log_loss(y_true: np.ndarray, y_pred: np.ndarray) -> float:
     """Log loss, aka logistic loss or cross-entropy loss."""
