f9871f62cb9920c3d39c70ffddc798a42e905fea
Arkhean <miens.julien@orange.fr>
2020-11-14 20:57:23 +0100

    debut decision tree

---
+++ pyfit/decisionTreeClassifier.py
@@ -0,0 +1,18 @@
+"""
+Decision Tree Classfier class
+"""
+
+import numpy as np
+
+class DecisionTreeClassifier:
+    """
+
+    """
+    def __init__(self, criterion: str, max_depth: int) -> None:
+        """"""
+
+    def fit(self, x: np.ndarray, y: np.ndarray) -> None:
+        """Build a decision tree classifier from the training set (X, y)."""
+
+    def predict(self, x: np.ndarray) -> np.ndarray:
+        """Predict class or regression value for X."""
