32d4be3ae1bde5f342652712f154f157c833b15d
Gwenaelle Cadic <gcadic@ensc.fr>
2020-11-19 17:10:01 +0000

    pylint+test

--- tests/test_linear_reg.py
+++ tests/test_linear_reg.py
@@ -1,9 +1,12 @@
-from pyfit.linear_reg import Linear_reg
+from pyfit.linear_reg import LinearReg
 import numpy as np
 
 def test_1D_lin_reg():
     x_train = np.array([[1], [2], [4], [6]])
-    y_train = np.array[[1], [2], [4], [6]])
-    model=Linear_reg(x_train,y_train,learning_rate=0.01,max_iter=10)
+    y_train = np.array([[1], [2], [4], [6]])
+    model=LinearReg(x_train,y_train,learning_rate=0.01,max_iter=10)
     x_entree = np.array([[3], [4], [5]])
-    y_pred=model.prevision(x_entree)
+    y_pred=model.predict(x_entree)
+    for i,element in enumerate(y_pred):
+        assert element[0] > x_entree[i][0] - 0.2
+        assert element[0] < x_entree[i][0] + 0.2
